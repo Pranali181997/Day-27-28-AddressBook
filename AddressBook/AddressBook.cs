28f07f9300d6ef9efe93349f28aa494b6470a4f3
Pranali <pranalilambat00@gmail.com>
2022-03-14 22:33:23 +0530

    [Pranali]Add:DeleteContact

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -8,11 +8,20 @@ namespace AddressBook
     {
         //creation of list
         public static List<Contact> People;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressBook"/> class.
+        /// </summary>
         public AddressBook()
         {
             People = new List<Contact>();
         }
 
+        /// <summary>
+        /// Finds the contact.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
         public Contact FindContact(string fname)
         {
             //finding the person's contact by first name
@@ -20,6 +29,18 @@ namespace AddressBook
             return contact;
         }
 
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        /// <param name="FirstName">The first name.</param>
+        /// <param name="LastName">The last name.</param>
+        /// <param name="Address">The address.</param>
+        /// <param name="City">The city.</param>
+        /// <param name="State">The state.</param>
+        /// <param name="ZipCode">The zip code.</param>
+        /// <param name="PhoneNumber">The phone number.</param>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
         public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
         {
             Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
@@ -36,5 +57,29 @@ namespace AddressBook
             else
                 return false;
         }
+
+        /// <summary>
+        /// Removes the contact.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool RemoveContact(string name)
+        {
+            //creation of object for contact
+            Contact c = FindContact(name);
+            //checks in c for the contact
+            //if it is true then contact will be removed
+            //otherwise returns false
+            if (c != null)
+            {
+                People.Remove(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
+
