dde3904a01997fb5004e6f29674d54788f27cedd
Pranali <pranalilambat00@gmail.com>
2022-03-14 22:25:42 +0530

    [Pranali]Add:EditContact

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        //creation of list
+        public static List<Contact> People;
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+
+        public Contact FindContact(string fname)
+        {
+            //finding the person's contact by first name
+            Contact contact = People.Find((person) => person.FirstName == fname);
+            return contact;
+        }
+
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            //finds contact and stores into result
+            Contact result = FindContact(FirstName);
+            //checks if result is empty
+            //then adds the contact and returns true
+            //else returns false
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -6,31 +6,38 @@ namespace AddressBook
 {
         class Contact
         {
-            public string FirstName { get; set; }
 
-            public string LastName { get; set; }
 
-            public string Address { get; set; }
+        public string FirstName { get; set; }
 
-            public string City { get; set; }
+        public string LastName { get; set; }
 
-            public string State { get; set; }
+        public string Address { get; set; }
 
-            public int ZipCode { get; set; }
+        public string City { get; set; }
 
-            public int PhoneNumber { get; set; }
+ 
+        public string State { get; set; }
 
-            public string Email { get; set; }
-            public Contact(string FirstName, string LastName, string Address, string City, string State, int ZipCode, int PhoneNumber, string Email)
-            {
-                this.FirstName = FirstName;
-                this.LastName = LastName;
-                this.Address = Address;
-                this.City = City;
-                this.State = State;
-                this.ZipCode = ZipCode;
-                this.PhoneNumber = PhoneNumber;
-                this.Email = Email;
-            }
+
+        public string ZipCode { get; set; }
+
+
+        public string PhoneNumber { get; set; }
+
+
+        public string Email { get; set; }
+
+        public Contact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.ZipCode = ZipCode;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
         }
     }
+}
